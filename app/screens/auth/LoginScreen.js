ddfd14d16090b3d3a86b18c3ebe6eb2fe5a5af41
mikuh21 <mikamsanchez21@gmail.com>
2026-07-27 19:35:47 +0800

    Fix Google Sign-In configuration

--- app/screens/auth/LoginScreen.js
+++ app/screens/auth/LoginScreen.js
@@ -22,7 +22,7 @@ import theme from '../../theme';
 GoogleSignin.configure({
   webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
   iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
-  offlineAccess: true,
+  offlineAccess: false,
 });
 
 const logoImage = require('../../../assets/auth/brewing-connections-logo.png');
