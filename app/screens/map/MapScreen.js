4baa9f65999a672ad9d82e95eae016685fd1807a
mikuh21 <mikamsanchez21@gmail.com>
2026-08-15 17:21:30 +0800

    Fix the marker number of stops in trail mode 4

--- app/screens/map/MapScreen.js
+++ app/screens/map/MapScreen.js
@@ -2740,7 +2740,7 @@ export default function MapScreen({ navigation, route }) {
                   zIndex={40}
                   anchor={{ x: 0.5, y: 0.5 }}
                 >
-                  <View
+                  <Animated.View
                     style={[
                       styles.trailMarker,
                       isCurrent && trailState === 'navigating'
@@ -2750,7 +2750,7 @@ export default function MapScreen({ navigation, route }) {
                     ]}
                   >
                     <Text style={styles.trailMarkerText}>{stop.number}</Text>
-                  </View>
+                  </Animated.View>
                 </Marker>
               );
             })
